cf42f08d43bee0fad69b0383780c3aa4703b0bc1
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-05-26 14:00:11 +0800

    Added seeder for address table

--- kita-src/database/seeders/DatabaseSeeder.php
+++ kita-src/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Address;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Address::factory()->create([
+            'barangay' => 'test brgy',
+            'city' => 'test city',
+            'province' => 'test province',
+            'region' => 'test region',
+        ]);
+
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'addressId' => 3423,
         ]);
     }
 }
