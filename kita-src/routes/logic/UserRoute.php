2ac777dbae4199b36e1a3a1d7fe4a76817238a47
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-05-28 20:45:13 +0800

    Tested. FINISHED

--- kita-src/routes/logic/UserRoute.php
+++ kita-src/routes/logic/UserRoute.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Logic\UserController;
 Route::controller(UserController::class)->group(function () 
 {
     Route::post('/users', 'store')->name('user.store');
-
+    
     Route::middleware(['auth', 'verified'])->group(function () 
     {
         Route::get('/users/me', 'me')->name('user.me');
