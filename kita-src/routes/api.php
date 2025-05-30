7ec0d5fdfd1d161ee87824aa574484d9c80e1311
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-05-30 13:41:26 +0800

    delete previous project. FINISHED

--- kita-src/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-require base_path('routes/logic/UserRoute.php');
--- kita-src/routes/auth.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Livewire\Volt\Volt;
