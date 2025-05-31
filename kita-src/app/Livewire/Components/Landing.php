9c3de7618eac8023b9d68c4ed77ce16fc87a71c4
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-01 00:26:47 +0800

    Created livewire component, landing. FINISHED

---
+++ kita-src/app/Livewire/Components/Landing.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Livewire\Components;
+
+use Livewire\Component;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Layout;
+
+
+#[Layout('livewire.layout.app')]
+#[Title('Kita')]
+class Landing extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.landing')
+            ->layoutData([
+                'bgImgUrl' => asset('/assets/bg-landing.jpeg'),
+            ]);
+    }
+}
