619530a4acb78dfabab7ef09ae1b07ffbf3f341f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-01 14:07:23 +0800

    Start the landing design. IN PROGRESS

--- kita-src/resources/views/livewire/components/landing.blade.php
+++ kita-src/resources/views/livewire/components/landing.blade.php
@@ -1,4 +1,10 @@
 
+@push('styles')
+    @vite('resources/css/landing.css')
+@endpush
+
 <div>
-    {{-- The Master doesn't talk, he acts. --}}
+    <div class="bg-dark-green-20 font-instrument">
+        <h1 class="font-instrument text-5xl font-bold text-outline-white">Hi, We're Kita — Your Smart Kiosk Companion</h1>
+    </div>
 </div>
