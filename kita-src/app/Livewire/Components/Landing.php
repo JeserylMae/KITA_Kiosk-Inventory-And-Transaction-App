3deb24bff18a144c7b7db3a7eeb18131a9e48b8c
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-01 12:01:32 +0800

    fixed layoutData(). FINISHED

--- kita-src/app/Livewire/Components/Landing.php
+++ kita-src/app/Livewire/Components/Landing.php
@@ -6,16 +6,22 @@ use Livewire\Component;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Layout;
 
+use function Livewire\Volt\layout;
 
 #[Layout('livewire.layout.app')]
 #[Title('Kita')]
 class Landing extends Component
 {
+    public function layoutData() 
+    {
+        return [
+            'bgImgUrl' => asset('/assets/bg-landing.jpeg'),
+        ];
+    }
+
     public function render()
     {
         return view('livewire.components.landing')
-            ->layoutData([
-                'bgImgUrl' => asset('/assets/bg-landing.jpeg'),
-            ]);
+             ->layoutData($this->layoutData());
     }
 }
