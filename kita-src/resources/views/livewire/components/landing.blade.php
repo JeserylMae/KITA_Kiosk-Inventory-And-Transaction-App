507625b946850292b0a47398f43ac67738605039
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-02 14:52:10 +0800

    Created navbar. IN PROGRESS

--- kita-src/resources/views/livewire/components/landing.blade.php
+++ kita-src/resources/views/livewire/components/landing.blade.php
@@ -3,8 +3,25 @@
     @vite('resources/css/landing.css')
 @endpush
 
-<div>
-    <div class="bg-dark-green-20 font-instrument">
-        <h1 class="font-instrument text-5xl font-bold text-outline-white">Hi, We're Kita — Your Smart Kiosk Companion</h1>
+<div class="main-section flex-col">
+    <div class="navbar-wrapper">
+        <button class="button-logo">
+            <img src="assets/kita-logo.png" alt="Kita Logo">
+        </button>
+        
+        <flux:navbar class="navbar">
+            <flux:navbar.item href="#" icon="users"> ABOUT US </flux:navbar.item>
+            <flux:navbar.item href="#" icon="briefcase"> SERVICES </flux:navbar.item>
+            <flux:navbar.item href="#" icon="command-line"> DEVELOPERS </flux:navbar.item>
+        </flux:navbar>
+    </div>
+
+    <div class="content-wrapper">
+
+        <div>
+            <h1 class="font-instrument text-5xl font-bold text-outline-bright-green">
+                Hi, We’re Kita — Your Smart Kiosk Companion
+            </h1>
+        </div>
     </div>
 </div>
