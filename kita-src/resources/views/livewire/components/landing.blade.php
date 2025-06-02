548cfdb30666ea0bbec4a951d6e4856132d230d3
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-02 21:25:29 +0800

    Added new elements. FINISHED

--- kita-src/resources/views/livewire/components/landing.blade.php
+++ kita-src/resources/views/livewire/components/landing.blade.php
@@ -4,7 +4,7 @@
 @endpush
 
 <div class="main-section flex-col">
-    <div class="navbar-wrapper">
+    <div class="navbar-wrapper flex-row-jstart-amid">
         <button class="button-logo">
             <img src="assets/kita-logo.png" alt="Kita Logo">
         </button>
@@ -14,14 +14,21 @@
             <flux:navbar.item href="#" icon="briefcase"> SERVICES </flux:navbar.item>
             <flux:navbar.item href="#" icon="command-line"> DEVELOPERS </flux:navbar.item>
         </flux:navbar>
+
+        <div class="flex-row-jstart-amid ml-auto mr-5 gap-2">
+            <flux:button size="sm" variant="ghost"> Log In </flux:button>
+            <flux:button size="sm" variant="primary" class="primary-button"> Sign Up </flux:button>
+        </div>
     </div>
 
     <div class="content-wrapper">
 
         <div>
-            <h1 class="font-instrument text-5xl font-bold text-outline-bright-green">
+            <h1 class="font-heading text-outline-bright-green">
                 Hi, We’re Kita — Your Smart Kiosk Companion
             </h1>
         </div>
     </div>
+
+    @fluxScripts
 </div>
