972b3e22e9bd3254a371c6e055329706ff575c04
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-03 21:31:04 +0800

    Fixed shadowing. FINISHED

--- kita-src/resources/views/livewire/layout/app.blade.php
+++ kita-src/resources/views/livewire/layout/app.blade.php
@@ -7,9 +7,9 @@
 </head>
 
 <body style="background-color: #2b3718">
-    <div class="w-screen mx-0 md:mx-auto lg:max-w-[90rem] h-screen shadow-2xl shadow-black" 
+    <div class="w-screen mx-0 md:mx-auto lg:max-w-[90rem] h-screen" 
          style="background-color: white">
-        <div class="bg-cover bg-right bg-no-repeat bg-fixed w-full h-max"
+        <div class="bg-cover bg-right bg-no-repeat bg-fixed w-full h-max shadow-2xl shadow-black"
              style="background-image: url('{{ $bgImgUrl }}')">
             
             {{ $slot }}
