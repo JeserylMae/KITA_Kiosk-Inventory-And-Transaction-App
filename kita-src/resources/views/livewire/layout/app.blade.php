d68d2836af7f9d311948be5dbdfdb60601560ab3
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-03 21:30:08 +0800

    Added new div. FINISHED

--- kita-src/resources/views/livewire/layout/app.blade.php
+++ kita-src/resources/views/livewire/layout/app.blade.php
@@ -6,12 +6,15 @@
     @livewireStyles
 </head>
 
-<body style="background-color: var(--color-dark-green-20)">
-    <div class="bg-cover bg-right bg-no-repeat bg-fixed w-screen mx-0 md:mx-auto lg:max-w-[90rem] h-max shadow-2xl shadow-black"
-         style="background-image: url('{{ $bgImgUrl }}')">
-        
-        {{ $slot }}
-
+<body style="background-color: #2b3718">
+    <div class="w-screen mx-0 md:mx-auto lg:max-w-[90rem] h-screen shadow-2xl shadow-black" 
+         style="background-color: white">
+        <div class="bg-cover bg-right bg-no-repeat bg-fixed w-full h-max"
+             style="background-image: url('{{ $bgImgUrl }}')">
+            
+            {{ $slot }}
+    
+        </div>
     </div>
     @livewireScripts
     @fluxScripts
