3ff1f3bb8d26100a96dd82776edb8655dbe49c1f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-03 15:14:02 +0800

    Added elements of the welcome page. FINISHED

--- kita-src/resources/views/livewire/components/landing.blade.php
+++ kita-src/resources/views/livewire/components/landing.blade.php
@@ -4,8 +4,9 @@
 @endpush
 
 <div class="main-section flex-col">
-    <div class="navbar-wrapper flex-row-jstart-amid">
-        <button class="button-logo">
+    {{-- At md(768px) remove this navbar --}}
+    <div class="navbar-wrapper flex-row-jstart-amid"> 
+        <button class="btn-logo active-btn">
             <img src="assets/kita-logo.png" alt="Kita Logo">
         </button>
         
@@ -16,17 +17,25 @@
         </flux:navbar>
 
         <div class="flex-row-jstart-amid ml-auto mr-5 gap-2">
-            <flux:button size="sm" variant="ghost"> Log In </flux:button>
-            <flux:button size="sm" variant="primary" class="primary-button"> Sign Up </flux:button>
+            <flux:button class="active-btn" size="sm" variant="ghost"> Log In </flux:button>
+            <flux:button class="active-btn primary-btn" size="sm" variant="primary"> Sign Up </flux:button>
         </div>
     </div>
 
-    <div class="content-wrapper">
+    <div class="content-wrapper px-0 py-3 sm:p-[1.25rem] md:my-14">
 
-        <div>
-            <h1 class="font-heading text-outline-bright-green">
+        <div class="content-wrapper glassy-green flex-col-jcenter-astart welcome-wrapper">
+            <h1 class="text-outline-bright-green">
                 Hi, We’re Kita — Your Smart Kiosk Companion
             </h1>
+
+            <h2 class="blury-white">A Kiosk Inventory & Transaction Application.</h2>
+
+            <p class="blury-white">
+                Manage your Sari-Sari Store with ease using Kita, the all-in-one inventory and transaction app designed for small business owners. Track sales, monitory stocks, and streamline daily operation — all from your phone or tablet. Simple. Fast. Reliable.
+            </p>
+
+            <flux:button class="active-btn primary-btn" icon="rocket-launch" variant="primary">Join Now</flux:button>
         </div>
     </div>
 
