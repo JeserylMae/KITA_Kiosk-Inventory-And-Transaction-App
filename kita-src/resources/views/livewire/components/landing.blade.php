dd982704e824da0c1bbf8927d74626871fc0ffbe
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-03 21:29:48 +0800

    Created the about us section. FINISHED

--- kita-src/resources/views/livewire/components/landing.blade.php
+++ kita-src/resources/views/livewire/components/landing.blade.php
@@ -3,15 +3,15 @@
     @vite('resources/css/landing.css')
 @endpush
 
+
 <div class="main-section flex-col">
-    {{-- At md(768px) remove this navbar --}}
     <div class="navbar-wrapper flex-row-jstart-amid"> 
         <button class="btn-logo active-btn">
             <img src="assets/kita-logo.png" alt="Kita Logo">
         </button>
         
         <flux:navbar class="navbar">
-            <flux:navbar.item href="#" icon="users"> ABOUT US </flux:navbar.item>
+            <flux:navbar.item href="#about" icon="users"> ABOUT US </flux:navbar.item>
             <flux:navbar.item href="#" icon="briefcase"> SERVICES </flux:navbar.item>
             <flux:navbar.item href="#" icon="command-line"> DEVELOPERS </flux:navbar.item>
         </flux:navbar>
@@ -29,13 +29,41 @@
                 Hi, We’re Kita — Your Smart Kiosk Companion
             </h1>
 
-            <h2 class="blury-white">A Kiosk Inventory & Transaction Application.</h2>
+            <h2 class="blurry-white">A Kiosk Inventory & Transaction Application.</h2>
 
-            <p class="blury-white">
+            <p class="blurry-white">
                 Manage your Sari-Sari Store with ease using Kita, the all-in-one inventory and transaction app designed for small business owners. Track sales, monitory stocks, and streamline daily operation — all from your phone or tablet. Simple. Fast. Reliable.
             </p>
 
             <flux:button class="active-btn primary-btn" icon="rocket-launch" variant="primary">Join Now</flux:button>
+        </div>
+
+        <div id="about" class="flex-col-jcenter-astart pt-4 gap-5"> 
+            <h2 class="glassy-green text-outline-bright-green py-2 px-6 tracking-wider"> About Us </h2>
+    
+            @component('components.partials.glassy-text-block')
+                @slot('blockTitle') Designed for Every Sari-Sari Store Owner @endslot
+                
+                @slot('imgUrl') {{ asset('assets/about-us-1.png') }} @endslot
+
+                Kita was built with the everyday Filipino store owner in mind. Whether you're running a small neighborhood shop or a growing kiosk, Kita simplifies complex inventory and sales tracking tasks into a seamless digital experience. No more manual logs or lost records — just smart tools that help you monitor stock levels, transactions, and sales trends with ease.
+            @endcomponent
+
+            @component('components.partials.glassy-text-block')
+                @slot('blockTitle') Real-Time Insights, Smarter Decisions @endslot
+
+                @slot('imgUrl') {{ asset('assets/about-us-2.png')}} @endslot
+
+                With Kita, you gain more than just record-keeping — you gain visibility. Generate detailed reports, analyze daily earnings, and visualize your store’s performance through charts and tables. Our system helps you make data-driven decisions, reduce stock shortages, and stay ahead of your day-to-day operations — all in just a few clicks.
+            @endcomponent
+
+            @component('components.partials.glassy-text-block')
+                @slot('blockTitle') Your Partner in Everyday Business @endslot
+
+                @slot('imgUrl') {{ asset('assets/about-us-3.png') }} @endslot
+
+                Kita isn’t just an app — it’s your daily companion in managing and growing your sari-sari store. From recording transactions to generating clear and organized sales summaries, Kita is built to support your routine. Whether you’re restocking shelves or reviewing weekly performance, Kita keeps everything organized so you can focus on what matters most: serving your community and growing your business.
+            @endcomponent
         </div>
     </div>
 
