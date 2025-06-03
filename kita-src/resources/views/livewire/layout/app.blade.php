d310332b318476b05561798c1ffe61ae7a46eec3
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-03 15:14:27 +0800

    Added responsiveness. FINISHED

--- kita-src/resources/views/livewire/layout/app.blade.php
+++ kita-src/resources/views/livewire/layout/app.blade.php
@@ -7,7 +7,7 @@
 </head>
 
 <body style="background-color: var(--color-dark-green-20)">
-    <div class="bg-cover bg-right bg-no-repeat bg-fixed w-full mx-auto lg:max-w-[90rem] h-max shadow-2xl shadow-black"
+    <div class="bg-cover bg-right bg-no-repeat bg-fixed w-screen mx-0 md:mx-auto lg:max-w-[90rem] h-max shadow-2xl shadow-black"
          style="background-image: url('{{ $bgImgUrl }}')">
         
         {{ $slot }}
