cd3e952e549ac5d25c9926c699ab77f350049589
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-06-04 14:16:56 +0800

    Removed example routes. FINISHED Added route for login page. FINISHED

--- kita-src/routes/web.php
+++ kita-src/routes/web.php
@@ -3,21 +3,8 @@
 use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Components\Landing;
+use App\Livewire\Components\Login;
 
 
 Route::get('/', Landing::class)->name('landing');
-
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/login', Login::class)->name('login');
