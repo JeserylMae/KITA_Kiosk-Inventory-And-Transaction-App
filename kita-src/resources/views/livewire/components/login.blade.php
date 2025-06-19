59708f4b24e66dd19133400f01ea321b299a06dd
JeserylMae <07comia.jeseryl131@gmail.com>
2025-06-19 11:44:01 +0800

    Added responsiveness. FINISHED

--- kita-src/resources/views/livewire/components/login.blade.php
+++ kita-src/resources/views/livewire/components/login.blade.php
@@ -5,7 +5,7 @@
 
 
 <div class="flex-row-jstart-amid w-full">
-    <div class="flex-col-jcenter-astart justify-between h-screen hidden sm:flex">
+    <div class="flex-col-jcenter-astart justify-between h-screen hidden md:flex">
         <button class="btn-logo active-btn">
             <img src="assets/kita-logo.png" alt="Kita Logo">
         </button>
@@ -13,9 +13,7 @@
         <img class="welcome-img" src="{{ asset('assets/login-welcome-character.png') }}" alt="Welcome to Kita">
     </div>
     
-    <div class="bg-styling flex-col-jcenter-acenter"
-         style="background-image: url({{ asset('assets/curvy-dark-green-frame.svg') }})">
-
+    <div class="bg-styling flex-col-jcenter-acenter ">
         <form class="flex-col-jcenter-acenter form-layout"
               wire:submit="login">
 
@@ -48,5 +46,7 @@
 
             <flux:button class="primary-btn active-btn px-16 border-0 !rounded-full">Log In</flux:button>
         </form>
+
+        <img src="{{ asset('assets/curvy-dark-green-frame.svg') }}" class="curvy-frame-styling">
     </div>
 </div>
