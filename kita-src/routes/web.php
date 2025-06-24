c45bf4e8fd822a2978e1e348db20639c75ce6274
JeserylMae <07comia.jeseryl131@gmail.com>
2025-06-24 11:40:06 +0800

    moved users route. FINISHED

--- kita-src/routes/web.php
+++ kita-src/routes/web.php
@@ -10,3 +10,10 @@ use App\Livewire\Components\Register;
 Route::get('/', Landing::class)->name('landing');
 Route::get('/login', Login::class)->name('login');
 Route::get('/register', Register::class)->name('register');
+
+Route::get('/csrf-token', function () {
+    return response()->json(['token' => csrf_token()]);
+});
+
+
+require base_path('routes/Logic/UserRoute.php');
