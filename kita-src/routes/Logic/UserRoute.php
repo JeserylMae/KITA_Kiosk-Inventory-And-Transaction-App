0bd4a63f38e2d587c8c567f6852c60d3d5faaa7d
JeserylMae <07comia.jeseryl131@gmail.com>
2025-06-24 11:40:20 +0800

    TEST.

--- kita-src/routes/Logic/UserRoute.php
+++ kita-src/routes/Logic/UserRoute.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Logic\UserController;
 
-
+// DOMAIN/users
 Route::controller(UserController::class)->group(function () 
 {
     Route::post('/users', 'store')->name('user.store');
     
     Route::middleware(['auth', 'verified'])->group(function () 
     {
-        Route::get('/users/me', 'me')->name('user.me');
         Route::get('/users', 'index')->name('user.index');
+        Route::get('/users/me', 'me')->name('user.me');
         Route::patch('/users/{user}', 'update')->name('user.update');
         Route::delete('/users/{user}', 'destroy')->name('user.destroy');
     });
