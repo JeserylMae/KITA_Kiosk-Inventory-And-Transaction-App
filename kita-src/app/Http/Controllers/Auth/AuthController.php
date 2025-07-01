eba37dcf546aff572dac9e4e874e1b9d3ac6b4ac
JeserylMae <07comia.jeseryl131@gmail.com>
2025-07-01 13:11:04 +0800

    created a separate controller for user signup and login

---
+++ kita-src/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreUserRequest;
+use App\Traits\HandlesEmailVerification;
+use App\Http\Controllers\Logic\UserController;
+
+class AuthController extends Authenticatable implements MustVerifyEmail
+{
+    public function __construct()
+    {
+        $this->user = new UserController();
+    }
+
+    /**
+     * Handle the incoming request.
+     */
+    public function signup(StoreUserRequest $request)
+    {
+        $response = $this->user->store($request);
+        $data = $response->getData();
+        $userId = $data->user->id;
+        
+        Auth::LoginUsingId($userId);
+
+        // $user = User::find($userId);
+        // $user->sendEmailVerificationNotification();
+        // To send a verification email, ensure your mail configuration is set up correctly in the .env file:
+        // MAIL_MAILER=smtp
+        // MAIL_HOST=smtp.mailtrap.io
+        // MAIL_PORT=2525
+        // MAIL_USERNAME=your_username
+        // MAIL_PASSWORD=your_password
+        // MAIL_ENCRYPTION=null
+        // MAIL_FROM_ADDRESS=no-reply@example.com
+        // MAIL_FROM_NAME="${APP_NAME}"
+        
+        return back()->with('success', 'User created successfully');
+    }
+}
