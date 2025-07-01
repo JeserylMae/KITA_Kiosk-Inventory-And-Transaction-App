79ed690525e01f0ad33f69be706dc4af82a3ba55
JeserylMae <07comia.jeseryl131@gmail.com>
2025-07-01 13:31:39 +0800

    Implemented login function. FINISHED

--- kita-src/resources/views/livewire/components/login.blade.php
+++ kita-src/resources/views/livewire/components/login.blade.php
@@ -15,18 +15,32 @@
     
     <div class="bg-styling flex-col-jcenter-acenter ml-auto mr-0">
         <form class="flex-col-jcenter-acenter form-layout ml-0 sm:ml-16"
-              wire:submit="login">
+              method="POST"
+              action="{{ route('user.login') }}">
+
+            @csrf
+            @if (session('success'))
+            <flux:callout variant="success" icon="check-circle" heading="{{ session('success') }}"
+                          class="w-100 bg-transparent"
+                          x-data="{ show: true }" 
+                          x-init="setTimeout(() => show = false, 3000)
+                                  setTimeout(() => window.location.href = '/dashboard', 3000)" 
+                          x-show="show"
+                          x-transition/>
+            @endif
 
             <h2 class="text-white"> Log in to your account.</h2>
 
             <flux:field>
                 <flux:label>Email</flux:label>
-                <flux:input type="email" clearable/>
+                <flux:input type="email" name="email" clearable/>
+                <flux:error name="email" />
             </flux:field>
 
              <flux:field>
                 <flux:label>Password</flux:label>
-                <flux:input type="password" viewable/>
+                <flux:input type="password" name="password" viewable/>
+                <flux:error name="password" />
             </flux:field>
 
             <div class="w-full gap-2 flex-col-jcenter-acenter items-end">
@@ -38,7 +52,7 @@
                 </p>
             </div>
 
-            <flux:button class="primary-btn active-btn px-16 border-0 !rounded-full">Log In</flux:button>
+            <flux:button class="primary-btn active-btn px-16 border-0 !rounded-full" type="submit">Log In</flux:button>
         </form>
         
         <img src="{{ asset('assets/curvy-dark-green-frame.svg') }}" class="curvy-frame-styling right-0">
