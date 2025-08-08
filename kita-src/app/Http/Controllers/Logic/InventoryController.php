3efd4b36176f53d3936f39bd713744f19598887a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-08 20:00:36 +0800

    Defined the controller methods for Inventory. FOR TESTING

--- kita-src/app/Http/Controllers/Logic/InventoryController.php
+++ kita-src/app/Http/Controllers/Logic/InventoryController.php
@@ -3,9 +3,50 @@
 namespace App\Http\Controllers\Logic;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Inventory;
+use App\Http\Requests\InventoryRequest;
 
 class InventoryController extends Controller
 {
-    //
+    public function index() 
+    {
+        $inventory = Inventory::all();
+
+        return response()->json([
+            'message' => 'Successfully retrieved all items in the inventory.',
+            'inventory' => $inventory
+        ], 200);
+    }
+
+    public function store(InventoryRequest $request)
+    {
+        $validated = $request->validated();
+
+        $inventory = Inventory::create($validated);
+
+        return response()->json([
+            'message' => 'Successfully created inventory items.',
+            'inventory' => $inventory
+        ], 201);
+    }
+
+    public function update(InventoryRequest $request, Inventory $inventory)
+    {
+        $validated = $request->validated();
+
+        $inventory->update($validated);
+
+        return response()->json([
+            'message' => 'Successfully updated the items in the inventory.'
+        ], 200);
+    }
+
+    public function destroy(Inventory $inventory)
+    {
+        $inventory->delete();
+
+        return response()->json([
+            'message' => 'Successfully deleted items in the inventory.'
+        ], 200);
+    }
 }
