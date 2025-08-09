ad9aeddfb7241b3a951c2747b4404216424c75e6
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-09 19:59:46 +0800

    Defined the attributes and relationships of the delivery model to other models. FINISHED

--- kita-src/app/Models/Delivery.php
+++ kita-src/app/Models/Delivery.php
@@ -6,5 +6,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class Delivery extends Model
 {
-    //
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var list<string>
+     */
+    protected $fillable = [
+        'seller_id',
+        'supplier_id', 
+        'product_id',
+        'quantity',
+        'delivery_date',
+        'wholesale_price',
+    ];
+
+    public function casts(): array
+    {
+        return [
+            'quantity' => 'integer',
+            'delivery_date' => 'datetime',
+            'wholesale_price' => 'float',
+        ];
+    }
+
+    public function user() 
+    {
+        return $this->belongsTo(User::class, 'seller_id');
+    }
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
