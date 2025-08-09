cdca7723e8a865a7cf29678392a293341b0139fc
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-09 19:58:59 +0800

    Defined the validation rules and failed validation handler for deliveries. FINISHED

--- kita-src/app/Http/Requests/DeliveryRequest.php
+++ kita-src/app/Http/Requests/DeliveryRequest.php
@@ -2,17 +2,20 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\RedirectResponse;
 
 class DeliveryRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
-    }
+    // public function authorize(): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Get the validation rules that apply to the request.
@@ -21,8 +24,36 @@ class DeliveryRequest extends FormRequest
      */
     public function rules(): array
     {
+        if(in_array($this->route()->getActionMethod(), ['store'])) {
+            return [
+                'seller_id' => 'required|integer|exists:users,id',
+                'supplier_id' => 'required|integer|exists:suppliers,id',
+                'product_id' => 'required|integer|exists:products,id',
+                'quantity' => 'required|integer|min:0',
+                'delivery_date' => 'required|date_format:Y-m-d H-i-s',
+                'wholesale_price' => 'required|float|min:0',
+            ];
+        }
         return [
-            //
+            'seller_id' => 'nullable|integer|exists:users,id',
+            'supplier_id' => 'nullable|integer|exists:suppliers,id',
+            'product_id' => 'nullable|integer|exists:products,id',
+            'quantity' => 'nullable|integer|min:0',
+            'delivery_date' => 'nullable|date_format:Y-m-d H-i-s',
+            'wholesale_price' => 'nullable|float|min:0'
         ];
+    }
+
+    public function failedValidation(Validator $validator): RedirectResponse
+    {
+        throw new HttpResponseException(redirect()
+            ->back()
+            ->withErrors($validator)
+            ->withInput()
+            ->with([
+                'success' => false,
+                'message' => 'Validation failed.'
+            ])
+        );
     }
 }
