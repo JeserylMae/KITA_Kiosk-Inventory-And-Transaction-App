71d90f22cd55e1f5f4fa31908efbccf4cbf28d02
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-09 13:55:24 +0800

    Defined the validation rules and failed validation handler of Product Supplier. FINISHED

--- kita-src/app/Http/Requests/ProductSupplierRequest.php
+++ kita-src/app/Http/Requests/ProductSupplierRequest.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ProductSupplierRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
-    public function authorize(): bool
-    {
-        return false;
-    }
+    // public function authorize(): bool
+    // {
+    //     return false;
+    // }
 
     /**
      * Get the validation rules that apply to the request.
@@ -21,8 +23,30 @@ class ProductSupplierRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            //
+        if(in_array($this->route()->getActionMethod(), ['store'])) {
+            return [
+                'seller_id' => 'required|integer|exists:users,id',
+                'supplier_id' => 'required|integer|exists:suppliers,id',
+                'product_id' => 'required|integer|exists:products,id',
+            ];
+        }
+       return [
+            'seller_id' => 'nullable|integer|exists:users,id',
+            'supplier_id' => 'nullable|integer|exists:suppliers,id',
+            'product_id' => 'nullable|integer|exists:products,id',
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(redirect()
+            ->back()
+            ->withErrors($validator)
+            ->withInput()
+            ->with([
+                'success' => false,
+                'message' => 'Validation failed.',
+            ])
+        );
     }
 }
