df6d504e35e7836c3b9c3f56a68d5355c09956bb
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-09 19:13:54 +0800

    Defined the fillable attributes and relationships of the transaction model to other models. FINISHED

--- kita-src/app/Models/Transaction.php
+++ kita-src/app/Models/Transaction.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var lits<string>
+     */
+    protected $fillable = [
+        'product_id',
+        'user_id', 
+        'transaction_datetime',
+        'selling_price',
+        'purchase_price',
+        'quantity',
+        'transaction_type',
+    ];
+
+    public function casts(): array 
+    {
+        return [
+            'selling_price' => 'float',
+            'purchase_price' => 'float',
+            'quantity' => 'integer',
+            'transaction_datetime' => 'datetime',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('users');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo('products');
+    }
 }
