87e732ca63b66550afccec47816cb0b7e6d628ce
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-09 13:55:48 +0800

    Defined the crud operations for Product Supplier. FINISHED

--- kita-src/app/Http/Controllers/Logic/ProductSupplierController.php
+++ kita-src/app/Http/Controllers/Logic/ProductSupplierController.php
@@ -3,9 +3,50 @@
 namespace App\Http\Controllers\Logic;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\ProductSupplierRequest;
+use App\Models\ProductSupplier;
 
 class ProductSupplierController extends Controller
 {
-    //
+    public function index()
+    {
+        $productSuppliers = ProductSupplier::all();
+
+        return response()->json([
+            'message' => 'Successfully retrieved all product suppliers.',
+            'productSuppliers' => $productSuppliers,
+        ], 200);
+    }
+
+    public function store(ProductSupplierRequest $request)
+    {
+        $validated = $request->validated();
+
+        $productSupplier = ProductSupplier::create($validated);
+
+        return response()->json([
+            'message' => 'Successfully created new product supplier.',
+            'productSupplier' => $productSupplier
+        ], 201);
+    }
+
+    public function update(ProductSupplierRequest $request, ProductSupplier $productSupplier)
+    {
+        $validated = $request->validated();
+
+        $productSupplier->update($validated);
+
+        return response()->json([
+            'message' => 'Successfully updated the product supplier.',
+        ], 200);
+    }
+
+    public function destroy(ProductSupplier $productSupplier)
+    {
+        $productSupplier->delete();
+
+        return response()->json([
+            'message' => 'Successfully deleted the product supplier.',
+        ], 200);
+    }
 }
