83dcc3144a5230a9ab09c8f27abf1e9cc09c2d87
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-10 13:04:28 +0800

    Created and defined the crud routes for Delivery. FINISHED

---
+++ kita-src/routes/Logic/DeliveryRoute.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Logic\DeliveryController;
+
+// DOMAIN/deliveries
+Route::controller(DeliveryController::class)->group(function ()
+{
+    Route::middleware(['auth', 'verified'])->group(function ()
+    {
+        Route::get('/deliveries', 'index')->name('delivery.index');
+        Route::post('/deliveries', 'store')->name('delivery.store');
+        Route::patch('/deliveries/{delivery}', 'update')->name('delivery.update');
+        Route::patch('/deliveries/{delivery}', 'destroy')->name('delivery.destroy');
+    });
+});
