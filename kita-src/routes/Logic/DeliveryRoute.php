a732def47977d7d0bdc14da30113c6c735fe9e1b
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-10 19:26:03 +0800

    Limited access based on roles. FINISHED

--- kita-src/routes/Logic/DeliveryRoute.php
+++ kita-src/routes/Logic/DeliveryRoute.php
@@ -8,9 +8,12 @@ Route::controller(DeliveryController::class)->group(function ()
 {
     Route::middleware(['auth', 'verified'])->group(function ()
     {
-        Route::get('/deliveries', 'index')->name('delivery.index');
-        Route::post('/deliveries', 'store')->name('delivery.store');
-        Route::patch('/deliveries/{delivery}', 'update')->name('delivery.update');
-        Route::patch('/deliveries/{delivery}', 'destroy')->name('delivery.destroy');
+        Route::get('/deliveries', 'index')->middleware('role:admin')->name('delivery.index');
+
+        Route::middleware('role:admin,seller')->group(function () {
+            Route::post('/deliveries', 'store')->name('delivery.store');
+            Route::patch('/deliveries/{delivery}', 'update')->name('delivery.update');
+            Route::patch('/deliveries/{delivery}', 'destroy')->name('delivery.destroy');
+        });
     });
 });
