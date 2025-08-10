b2c537a0f76ef08ba1c27d1de0e16958d6cdd951
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-10 11:50:47 +0800

    Created and defined crud routes for products. FINISHED

---
+++ kita-src/routes/Logic/ProductRoute.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Logic\ProductsController;
+use App\Models\Product;
+
+// DOMAIN/products
+Route::controller(Product::class)->group(function ()
+{
+    Route::middleware(['auth', 'verified'])->group(function () 
+    {
+        Route::get('/products', 'index')->name('product.index');
+        Route::post('/products', 'store')->name('product.store');
+        Route::patch('/proucts/{product}', 'update')->name('product.update');
+        Route::delete('/products/{product}', 'destroy')->name('product.destroy');
+    });
+});
