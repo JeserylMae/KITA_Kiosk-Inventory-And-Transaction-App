f9855f66b57815970814f3dec82cc77b084eeeb8
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-10 18:46:55 +0800

    Added route middlewares. FINISHED

--- kita-src/routes/Logic/ProductRoute.php
+++ kita-src/routes/Logic/ProductRoute.php
@@ -9,9 +9,13 @@ Route::controller(Product::class)->group(function ()
 {
     Route::middleware(['auth', 'verified'])->group(function () 
     {
-        Route::get('/products', 'index')->name('product.index');
-        Route::post('/products', 'store')->name('product.store');
-        Route::patch('/proucts/{product}', 'update')->name('product.update');
-        Route::delete('/products/{product}', 'destroy')->name('product.destroy');
+        Route::get('/products', 'index')->middleware('role:admin')->name('product.index');
+
+        Route::middleware(['role:admin,seller'])->group(function () 
+        {
+            Route::post('/products', 'store')->name('product.store');
+            Route::patch('/proucts/{product}', 'update')->name('product.update');
+            Route::delete('/products/{product}', 'destroy')->name('product.destroy');
+        });
     });
 });
