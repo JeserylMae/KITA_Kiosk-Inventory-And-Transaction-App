c33e13bc48fa49c43188e1275a47c941309381e9
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-10 18:35:36 +0800

    Added additional middleware for validating the roles. FINISHED

--- kita-src/routes/Logic/UserRoute.php
+++ kita-src/routes/Logic/UserRoute.php
@@ -8,7 +8,7 @@ Route::controller(UserController::class)->group(function ()
 {
     Route::post('/users', 'store')->name('user.store');
     
-    Route::middleware(['auth', 'verified'])->group(function () 
+    Route::middleware(['auth', 'verified', 'role:admin,seller'])->group(function () 
     {
         Route::get('/users', 'index')->name('user.index');
         Route::get('/users/me', 'me')->name('user.me');
