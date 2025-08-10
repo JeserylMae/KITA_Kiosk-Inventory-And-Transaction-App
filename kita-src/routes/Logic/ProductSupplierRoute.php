3794cfa44b34b2c8d1aae30cc194cc314aa51c3c
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-10 12:48:30 +0800

    Created and defined the crud routes for Product Supplier. FINISHED

---
+++ kita-src/routes/Logic/ProductSupplierRoute.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Logic\ProductSupplierController;
+
+// DOMAIN/product-suppliers
+Route::controller(ProductSupplierController::class)->group(function () 
+{
+    Route::middleware(['auth', 'verified'])->group(function ()
+    {
+        Route::get('/product-suppliers', 'index')->name('product-supplier.index');
+        Route::post('/product-suppliers', 'store')->name('product-supplier.store');
+        Route::patch('/product-suppliers/{productSupplier}', 'update')->name('product-supplier.update');
+        Route::delete('/product-suppliers/{productSupplier}', 'destroy')->name('product-supplier.destroy');
+    });
+});
