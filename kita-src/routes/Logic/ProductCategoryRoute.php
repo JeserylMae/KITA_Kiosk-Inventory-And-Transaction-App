4d5012670c5f4552f72d6eb95db9609f1f6146a3
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-11 18:28:16 +0800

    Fixed typos. FINISHED

--- kita-src/routes/Logic/ProductCategoryRoute.php
+++ kita-src/routes/Logic/ProductCategoryRoute.php
@@ -10,7 +10,7 @@ Route::controller(ProductCategoryController::class)->group(function ()
     {
         Route::get('/product-categories', 'index')->middleware('role:admin,owner,employee')->name('product-categories.index');
         
-        Route::middlerware('role:admin,owner')->group(function () {
+        Route::middleware('role:admin,owner')->group(function () {
             Route::patch('/product-categories/{productCategory}', 'update')->name('product-categories.update');
             Route::post('/product-categories', 'store')->name('product-categories.store');
             Route::delete('/product-categories/{productCategory}', 'destroy')->name('product-categories.destroy');
