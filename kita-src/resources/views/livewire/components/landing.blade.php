08c1d15c7f47ae7778c870f52f074e394debdc76
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-12 00:06:33 +0800

    Added flush message & sign up functionality. IN PROGRESS
    ``
    ERROR: Not flash nor error message is displayed.
    ``

--- kita-src/resources/views/livewire/components/landing.blade.php
+++ kita-src/resources/views/livewire/components/landing.blade.php
@@ -13,7 +13,7 @@
 
         <flux:field>
           <flux:label>Email</flux:label>
-          <flux:input wire:model="email" type="email" />
+          <flux:input name="email" type="email" />
           <flux:error name="email" />
         </flux:field>
 
@@ -33,25 +33,37 @@
     <br><br>
 
     <div class="p-5 border-1 rounded-md w-max">
-      <form method="POST" action="{{ route('user.store') }}" class="w-max">
+      <form method="POST" action="{{ route('user.signup') }}" class="w-max">
         @csrf
+
+        @if (session('success'))
+          <flux:callout icon="check-circle" variant="success" 
+            x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 3000)"
+            x-show="show"
+            x-transition>
+            <flux:callout.heading>You're all set!</flux:callout.heading>
+            <flux:callout.text>{{ session('success') }}</flux:callout.text>
+            {{-- <flux:callout.text>Registration completed successfully.</flux:callout.text> --}}
+          </flux:callout>
+        @endif
 
         <h3>SIGN UP</h3>
 
         <div class="flex flex-row gap-3">
           <flux:field>
             <flux:label>Last Name</flux:label>
-            <flux:input wire:model="last_name" clearable />
+            <flux:input name="last_name" clearable />
             <flux:error name="last_name" />
           </flux:field>
           <flux:field>
             <flux:label>First Name</flux:label>
-            <flux:input wire:model="first_name" clearable />
+            <flux:input name="first_name" clearable />
             <flux:error name="first_name" />
           </flux:field>
           <flux:field>
             <flux:label badge="Optional">Middle Name</flux:label>
-            <flux:input wire:model="middle_name" clearable />
+            <flux:input name="middle_name" clearable />
             <flux:error name="middle_name" />
           </flux:field>
         </div>
@@ -59,20 +71,20 @@
         <div>
           <flux:field>
             <flux:label>Email</flux:label>
-            <flux:input wire:model="email" type="email" clearable />
+            <flux:input name="email" type="email" clearable />
             <flux:error name="email" />
           </flux:field>
   
           <flux:field>
             <flux:label>Password</flux:label>
-            <flux:input type="password" viewable />
+            <flux:input name="password" type="password" viewable />
             <flux:error name="password" />
             <flux:description>Must be at least 8 characters long, include an uppercase letter, a number, and a special character.</flux:description>
           </flux:field>
 
           <flux:field>
             <flux:label>Confirm Password</flux:label>
-            <flux:input type="password" viewable />
+            <flux:input name="password_confirmation" type="password" viewable />
             <flux:error name="confirm_password" />
           </flux:field>
         </div>
@@ -84,23 +96,26 @@
             <flux:legend>Address</flux:legend>
 
             <div class="space-y-6">
-              <flux:input label="House Number" placeholder="0000" class="max-w-sm" badge="Optional"/>
-              <flux:input label="Street" placeholder="123 Main Street" class="max-w-sm" badge="Optional"/>
+              <flux:input name="house_number" label="House Number" placeholder="0000" class="max-w-sm" badge="Optional"/>
+              <flux:input name="street" label="Street" placeholder="123 Main Street" class="max-w-sm" badge="Optional"/>
 
               <div class="grid grid-cols-2 gap-x-4">
-                <flux:input label="Barangay" badge="Optional" />
-                <flux:input label="City" />
-                <flux:input label="State / Province" />
-                <flux:input label="Postal / Zip code" />
-                <flux:select label="Country">
+                <flux:input name="barangay" label="Barangay" badge="Optional" />
+                <flux:input name="city" label="City" />
+                <flux:input name="province" label="State / Province" />
+                <flux:input name="postal_code" label="Postal / Zip code" />
+                {{-- <flux:select label="Country">
                     <option selected>United States</option>
                     <option>Philippines</option>
                     <!-- ... -->
-                </flux:select>
+                </flux:select> --}}
               </div>
             </div>
           </flux:fieldset>
         </div>
+
+        <input type="hidden" name="role" value="owner" />
+        {{-- <flux:input name="role" value="owner" hidden/> --}}
 
         <br>
 
