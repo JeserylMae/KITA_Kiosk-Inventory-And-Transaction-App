06b3ec558910dc267e2ef762d7c217c9e555177a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-12 17:48:22 +0800

    Added links to login and signup page. FINISHED

--- kita-src/resources/views/livewire/components/landing.blade.php
+++ kita-src/resources/views/livewire/components/landing.blade.php
@@ -1,3 +1,10 @@
 <main>
-    <h1>Welcome to Kita!</h1>
+    <div>
+
+        <div>
+            <a class="btn-link" href="/"> Log In </a>
+            <a class="btn-link btn-primary" href="{{ route('register') }}"> Sign Up </a>
+        </div>
+    </div>
+    <flux:heading class="" size="xl">Welcome to Kita!</flux:heading>
 </main>
