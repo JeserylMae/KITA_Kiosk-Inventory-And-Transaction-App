21d07144ccc15b8bf95c4b84ab61d789b1fec34f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-12 16:47:59 +0800

    Fixed all errors in the authorize function. FINISHED

--- kita-src/app/Http/Requests/DeliveryRequest.php
+++ kita-src/app/Http/Requests/DeliveryRequest.php
@@ -12,10 +12,10 @@ class DeliveryRequest extends FormRequest
     /**
      * Determine if the user is authorized to make this request.
      */
-    // public function authorize(): bool
-    // {
-    //     return false;
-    // }
+    public function authorize(): bool
+    {
+        return true;
+    }
 
     /**
      * Get the validation rules that apply to the request.
