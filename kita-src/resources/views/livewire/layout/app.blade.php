8acdf9eb61499046236a46288c76f48323e83427
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-16 00:28:06 +0800

    Started creating dashboard, IN PROGRESS

--- kita-src/resources/views/livewire/layout/app.blade.php
+++ kita-src/resources/views/livewire/layout/app.blade.php
@@ -7,14 +7,21 @@
     @fluxAppearance
 </head>
 
-<body>
-    <div>
-        <div class="p-5">
-
-            {{ $slot }}
-    
+<body class="bg-[var(--color-background)] w-full h-max">
+    <div class="card border-t-0 w-full">
+        <div class="navbar">
+            <div class="stack-center">
+                <div class="layout inline-between">
+                    <div class="inline-between">
+                        <button>Kita</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
+    <div> {{ $slot }} </div>
+    
     @livewireScripts
     @fluxScripts
 </body>
