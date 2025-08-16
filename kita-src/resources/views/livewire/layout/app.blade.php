03f7c5140252dd3c868de4c0c3a8e9563ed03fb3
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-16 12:39:00 +0800

    Added navbar links. IN PROGRESS

--- kita-src/resources/views/livewire/layout/app.blade.php
+++ kita-src/resources/views/livewire/layout/app.blade.php
@@ -1,28 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @include('partials.head', ['title' => $title])
-    @stack('styles')
-    @livewireStyles
-    @fluxAppearance
+   @include('partials.head', ['title' => $title])
+   @stack('styles')
+   @livewireStyles
+   @fluxAppearance
 </head>
 
 <body class="bg-[var(--color-background)] w-full h-max">
-    <div class="card border-t-0 w-full">
-        <div class="navbar">
-            <div class="stack-center">
-                <div class="layout inline-between">
-                    <div class="inline-between">
-                        <button>Kita</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <div> {{ $slot }} </div>
-    
-    @livewireScripts
-    @fluxScripts
+   <div>
+      <div class="">
+         <button class="flex flex-row justify-center items-center gap-3 px-5 py-2">
+            <img src="{{ asset('favicon.svg') }}" width="20px">
+            <span class="font-medium text-[var(--color-foreground)]">Kita</span>
+         </button>
+
+         <livewire:components.button href="/" icon="home" label="Home" variant="link"/>
+         <livewire:components.button icon="calculate" label="Calculator" variant="link"/>
+         <livewire:components.button icon="notifications" label="Notifications" variant="link" badge="2"/>
+      </div>
+
+      <div>
+         <button class="btn btn-primary gap-1 px-2">
+            <span class="material-symbols-outlined !text-[20px]">add</span>
+            <span class="font-thin">|</span>
+            <span class="material-symbols-outlined">arrow_drop_down</span>
+         </button>
+
+         <button class="flex flex-row justify-center items-center gap-3 px-3 py-2 bg-[var(--color-muted)] text-[var(--color-muted-foreground)] rounded-xl rounded-br-none">
+            <span class="material-symbols-outlined">search</span>
+            <span>Search</span>
+         </button>
+      </div>
+   </div>
+
+   <div> {{ $slot }} </div>
+   
+   @livewireScripts
+   @fluxScripts
 </body>
 </html>
