468a7acf05da833ad7723b0ac1d8db09536df2d4
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-16 17:55:04 +0800

    Added properties. FINISHED

--- kita-src/resources/views/livewire/components/button.blade.php
+++ kita-src/resources/views/livewire/components/button.blade.php
@@ -1,11 +1,15 @@
 
 {{-- Success is as dangerous as failure. --}}
 
-@if($variant === 'link')
-    <a href="{{ $href }}" class="btn btn-ghost relative " . {{ $style }}>
-        <span class="material-symbols-outlined !text-[20px]">{{ $icon }}</span>
+@if ($variant === 'link')
+    <a href="{{ $href }}" class="btn btn-ghost relative {{ $style }}">
+        @if ($icon) 
+            <span class="material-symbols-outlined !text-[20px]">{{ $icon }}</span> 
+        @endif
 
-        @if ($label) <span @class(['my-0', $size, ])> {{ $label }} </span> @endif
+        @if ($label) 
+            <span @class(['my-0', $size, ])> {{ $label }} </span> 
+        @endif
 
         @if($badge)
             <span class="absolute top-2.5 -right-3 bg-[var(--color-muted)] 
@@ -13,13 +17,17 @@
                 {{ $badge }} 
             </span>
         @endif
-    </a>
+    </a>    
 @else 
-    <button @class([ 'btn', 'btn-' . $variant, $style, 'btn-disabled' => $disabled, ])>
+    <button @class([ 'btn', 'btn-' . $variant, $style, 'btn-disabled' => $disabled, ])>    
         <span class="material-symbols-outlined !text-[20px]">{{ $icon }}</span>
-
-        @if ($label) <span @class([ $size, ])> {{ $label }} </span> @endif
-
-        @if ($trailing) <span class="material-symbols-outlined !text-[20px]">{{ $trailing }}</span> @endif
+        
+        @if ($label) 
+            <span @class([ $size, ])> {{ $label }} </span> 
+        @endif
+        
+        @if ($trailing) 
+            <span class="material-symbols-outlined !text-[20px]">{{ $trailing }}</span> 
+        @endif
     </button>
 @endif
