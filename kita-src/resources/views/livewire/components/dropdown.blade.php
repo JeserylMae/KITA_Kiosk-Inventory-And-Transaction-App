3464620f425d529d7f9bf98e7d49e8604afb2331
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-16 17:50:50 +0800

    Defined dropdrown. IN PROGRESS

--- kita-src/resources/views/livewire/components/dropdown.blade.php
+++ kita-src/resources/views/livewire/components/dropdown.blade.php
@@ -1,3 +1,12 @@
 <div>
     {{-- Do your work, then step back. --}}
+    @if ($image) 
+        <img src="{{ asset($image) }}" alt="{{ $description }}" @class([$style,])>
+    @endif
+
+    <label for="{{ $name }}">{{ $label }}</label>
+
+    <select name="{{ $name }}" id="{{ $id }}">
+        <option></option>
+    </select>
 </div>
