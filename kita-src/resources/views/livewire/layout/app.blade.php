92b9202f2f518f3d29457cee030aa59ad53dce02
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-16 13:44:30 +0800

    Changed elements to the defined livewire component. IN PROGRESS

--- kita-src/resources/views/livewire/layout/app.blade.php
+++ kita-src/resources/views/livewire/layout/app.blade.php
@@ -9,29 +9,25 @@
 
 <body class="bg-[var(--color-background)] w-full h-max">
 
-   <div>
-      <div class="">
-         <button class="flex flex-row justify-center items-center gap-3 px-5 py-2">
-            <img src="{{ asset('favicon.svg') }}" width="20px">
-            <span class="font-medium text-[var(--color-foreground)]">Kita</span>
-         </button>
-
-         <livewire:components.button href="/" icon="home" label="Home" variant="link"/>
-         <livewire:components.button icon="calculate" label="Calculator" variant="link"/>
-         <livewire:components.button icon="notifications" label="Notifications" variant="link" badge="2"/>
-      </div>
-
-      <div>
-         <button class="btn btn-primary gap-1 px-2">
-            <span class="material-symbols-outlined !text-[20px]">add</span>
-            <span class="font-thin">|</span>
-            <span class="material-symbols-outlined">arrow_drop_down</span>
-         </button>
-
-         <button class="flex flex-row justify-center items-center gap-3 px-3 py-2 bg-[var(--color-muted)] text-[var(--color-muted-foreground)] rounded-xl rounded-br-none">
-            <span class="material-symbols-outlined">search</span>
-            <span>Search</span>
-         </button>
+   <div class="stack-center card">
+      <div class="navbar inline-between ">
+         <div class="inline-between">
+            <button class="flex flex-row justify-center items-center gap-3 px-5 py-2">
+               <img src="{{ asset('favicon.svg') }}" width="20px">
+               <span class="font-medium text-[var(--color-foreground)]">Kita</span>
+            </button>
+   
+            <livewire:components.button href="/" icon="home" label="Home" variant="link" size="text-sm"/>
+            <livewire:components.button icon="calculate" label="Calculator" variant="link" size="text-sm"/>
+            <livewire:components.button icon="notifications" label="Notifications" variant="link" badge="2" size="text-sm"/>
+         </div>
+   
+         <div class="inline-between">
+            <livewire:components.button icon="add" variant="primary" label="|" trailing="arrow_drop_down" size="text-sm" style="gap-1 rounded-br-none"/>
+            <livewire:components.button icon="search" label="Search" style="rounded-lg rounded-br-none ml-2" size="text-sm"/>
+            <livewire:components.button icon="info" variant="ghost"/>
+            <livewire:components.button icon="settings" variant="ghost"/>
+         </div>
       </div>
    </div>
 
