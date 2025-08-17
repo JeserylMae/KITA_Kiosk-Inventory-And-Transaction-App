67243005f54cde79641c7a1127776de4794fa44b
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-17 15:16:08 +0800

    moved sidebar and navbar. FINISHED

--- kita-src/resources/views/livewire/layout/app.blade.php
+++ kita-src/resources/views/livewire/layout/app.blade.php
@@ -10,38 +10,21 @@
 <body class="bg-[var(--color-background)] w-full h-max">
 
    <div class="stack-center card">
-      <div class="navbar inline-between ">
-         <div class="inline-between">
-            <button class="flex flex-row justify-center items-center gap-3 px-5 py-2">
-               <img src="{{ asset('favicon.svg') }}" width="20px">
-               <span class="font-medium text-[var(--color-foreground)]">Kita</span>
-            </button>
+      <livewire:navigations.navbar/>
+   </div>
 
-            <livewire:components.button href="/" icon="home" label="Home" variant="link" size="text-sm" class="navlink"/>
-            <livewire:components.button icon="calculate" label="Calculator" variant="link" size="text-sm" class="navlink"/>
-            <livewire:components.button icon="notifications" label="Notifications" variant="link" badge="2" size="text-sm" class="navlink"/>
-         </div>
-   
-         <div class="inline-between">
-            <livewire:components.button icon="add" variant="primary" label="|" trailing="arrow_drop_down" size="text-sm" class="gap-1 rounded-br-none"/>
-            <livewire:components.button icon="search" label="Search" class="rounded-lg rounded-br-none pr-5 mx-2" size="text-sm"/>
-            <livewire:components.button icon="info" variant="ghost"/>
-            <livewire:components.button icon="settings" variant="ghost"/>
+   <div class="stack-center">
+      <div class="layout">
+         <div class="inline-start">
+            <div class="w-1/4"> <livewire:navigations.sidebar/> </div>
 
-            <flux:dropdown position="bottom" align="end" class="ml-1">
-               <flux:profile avatar="{{ asset('favicon.svg') }}" name="Olivia Martin" />
-
-               <flux:navmenu>
-                  <flux:navmenu.item href="#" icon="user">Account</flux:navmenu.item>
-                  <flux:navmenu.item href="#" icon="building-storefront">Profile</flux:navmenu.item>
-                  <flux:navmenu.item href="#" icon="arrow-right-start-on-rectangle">Logout</flux:navmenu.item>
-               </flux:navmenu>
-            </flux:dropdown>
+            <div class="w-full pl-8"> 
+               <h2>hskdsh</h2>
+               {{ $slot }} 
+            </div>
          </div>
       </div>
    </div>
-
-   <div> {{ $slot }} </div>
    
    @livewireScripts
    @fluxScripts
