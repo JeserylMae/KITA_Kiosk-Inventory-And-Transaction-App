6b19b0a5403f5997ff06bec967674d99851ef0f8
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-21 12:40:12 +0800

    Fixed navigations. FINISHED

--- kita-src/resources/views/livewire/navigations/sidebar.blade.php
+++ kita-src/resources/views/livewire/navigations/sidebar.blade.php
@@ -3,42 +3,47 @@
     <x-button 
         id="dashboard" 
         variant="filled" 
-        wire:click="setActiveButton('dashboard')"
-        class="{{ $currentPage === 'dashboard' ? 'sidebar-item-active' : 'sidebar-item' }}"
+        wire:navigate
+        href="{{ route('page.dashboard') }}"
+        class="{{ Route::is('page.dashboard') ? 'sidebar-item-active' : 'sidebar-item' }}"
         > Dashboard 
     </x-button>
 
     <x-button
         id="inventory"
         variant="filled"
-        wire:click="setActiveButton('inventory')"
-        trailing="{{ $currentPage === 'inventory' ? 'arrow_drop_down' : 'arrow_right' }}"
-        class="{{ $currentPage === 'inventory' ? 'sidebar-item-active trailing' : 'sidebar-item' }}"
+        wire:navigate
+        href="{{ route('page.inventory') }}"
+        trailing="{{ Route::is('page.inventory') ? 'arrow_drop_down' : 'arrow_right' }}"
+        class="{{ Route::is('page.inventory') ? 'sidebar-item-active trailing' : 'sidebar-item' }}"
         > Inventory  
     </x-button>
 
     <x-button
         id="sales"
         variant="filled"
-        wire:click="setActiveButton('sales')"
-        class="{{ $currentPage === 'sales' ? 'sidebar-item-active' : 'sidebar-item' }}"
+        wire:navigate
+        href="{{ route('page.sales') }}" 
+        class="{{ Route::is('page.sales') ? 'sidebar-item-active' : 'sidebar-item' }}"
         > Sales 
     </x-button>
 
     <x-button
         id="expenses"
         variant="filled"
-        wire:click="setActiveButton('expenses')"
-        class="{{ $currentPage === 'expenses' ? 'sidebar-item-active' : 'sidebar-item' }}"
+        wire:navigate
+        href="{{ route('page.expenses') }}"
+        class="{{ Route::is('page.expenses') ? 'sidebar-item-active' : 'sidebar-item' }}"
         > Expenses 
     </x-button>
 
     <x-button
         id="reports"
         variant="filled"
-        wire:click="setActiveButton('reports')"
-        trailing="{{ $currentPage === 'reports' ? 'arrow_drop_down' : 'arrow_right' }}"
-        class="{{ $currentPage === 'reports' ? 'sidebar-item-active' : 'sidebar-item' }}"
+        wire:navigate
+        href="{{ route('page.reports') }}"
+        trailing="{{ Route::is('page.reports') ? 'arrow_drop_down' : 'arrow_right' }}"
+        class="{{ Route::is('page.reports') ? 'sidebar-item-active' : 'sidebar-item' }}"
         > Reports & Analytics 
     </x-button>
 </div>
