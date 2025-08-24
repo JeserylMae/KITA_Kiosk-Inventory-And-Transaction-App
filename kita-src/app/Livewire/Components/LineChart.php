2f7b9b06191694d07d73e4833d764e0e8fe638c5
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-24 11:52:39 +0800

    Init Line Chart. FINISHED

---
+++ kita-src/app/Livewire/Components/LineChart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Components;
+
+use Livewire\Component;
+
+class LineChart extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.line-chart');
+    }
+}
