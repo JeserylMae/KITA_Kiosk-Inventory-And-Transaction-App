b3ffbf305a82f6494acd269e6f5299d7ff64c128
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-29 17:27:13 +0800

    Fixed breadcrumbs, added stylings. FINISHED

--- kita-src/resources/views/livewire/components/breadcrumbs.blade.php
+++ kita-src/resources/views/livewire/components/breadcrumbs.blade.php
@@ -1,7 +1,25 @@
-{{-- Nothing in the world is as soft and yielding as water. --}}
+
+{{-- 
+    Sample Usage
+    <div>
+        <livewire:components.breadcrumbs :path="[
+            ['name' => 'Home', 'link' => 'page.dashboard'],
+            ['name' => 'Inventory', 'link' => 'page.inventory']
+        ]"/>
+    </div>
+--}}
 
 <div class="inline-start w-max h-max">
-    @foreach($paths as $path)
-        <a href="{{ $path['link'] }}">{{ $path['name'] }}</a>
-    @endforeach
+    @for ($i = 0; $i < count($path); $i++)
+
+        <a  class="text-base text-[var(--color-muted-foreground)] hover:underline underline-offset-4"
+            href="{{ route($path[$i]['link']) }}" 
+            wire:navigate> 
+            {{ $path[$i]['name'] }}
+        </a>
+    
+        @if ($i !== count($path)-1)
+            <p class="mx-2 text-base text-[var(--color-muted-foreground)]"> > </p>
+        @endif
+    @endfor
 </div>
