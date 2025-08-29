b9de59bf5c9691101d7e2463b1941cd69d6f824e
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-29 16:15:48 +0800

    Added main components of products page. IN PROGRESS

--- kita-src/resources/views/livewire/pages/products.blade.php
+++ kita-src/resources/views/livewire/pages/products.blade.php
@@ -1,3 +1,12 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+{{-- If your happiness depends on money, you will never be happy with yourself. --}}
+
+<div class="stack-start container">
+    <div class="stack-start">
+        <h1>Inventory</h1>
+        {{-- breadcrumbs --}}
+    </div>
+
+    <div class="w-full">
+
+    </div>
 </div>
