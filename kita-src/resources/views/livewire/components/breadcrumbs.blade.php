0aaef484505f8a9967a6a1f882d2bc641357462a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-08-29 16:53:17 +0800

    Defined breadcumbs components. IN PROGRESS

--- kita-src/resources/views/livewire/components/breadcrumbs.blade.php
+++ kita-src/resources/views/livewire/components/breadcrumbs.blade.php
@@ -1,3 +1,7 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+{{-- Nothing in the world is as soft and yielding as water. --}}
+
+<div class="inline-start w-max h-max">
+    @foreach($paths as $path)
+        <a href="{{ $path['link'] }}">{{ $path['name'] }}</a>
+    @endforeach
 </div>
