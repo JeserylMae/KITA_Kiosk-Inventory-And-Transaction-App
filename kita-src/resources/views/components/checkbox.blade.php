8f6325106498c8aa92aad10df96694faddfc4001
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-09-01 22:38:45 +0800

    Defined the structure of the checkbox. FINISHED

---
+++ kita-src/resources/views/components/checkbox.blade.php
@@ -0,0 +1,9 @@
+@props([
+    'id' => 'djfd48j'
+])
+
+
+<label for="{{ $id }}" class="relative">
+    <input type="checkbox" name="{{ $id }}" id="{{ $id }}" class="checkbox">
+    <span class="material-symbols-outlined checkmark">check</span>
+</label>
