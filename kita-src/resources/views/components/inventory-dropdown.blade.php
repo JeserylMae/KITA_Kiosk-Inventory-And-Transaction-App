c6ec0807a68c92a7517e7a2f68909a0f43a4ffaa
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-09-02 01:00:38 +0800

    Defined inventory tabs button. FINISHED

--- kita-src/resources/views/components/inventory-dropdown.blade.php
+++ kita-src/resources/views/components/inventory-dropdown.blade.php
@@ -1,3 +1,40 @@
-<div>
-    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
+ 
+@props([
+    'class',
+    'activeTabButton' => 'none',
+    'tab' => false
+])
+
+<div class="{{ $class }} w-full pl-5 ml-3 border-l border-l-[var(--color-border)]">
+    <x-button 
+        id="products" 
+        variant="ghost" 
+        class="{{ ($activeTabButton === 'products') ? 'btn-primary' : 'btn-ghost' }}"
+        @click="Livewire.dispatchTo('pages.inventory', 'switchTab', { button: 'products' })"
+        > Products 
+    </x-button>
+
+    <x-button 
+        id="product-category" 
+        variant="ghost" 
+        class="{{ ($activeTabButton === 'product-category') ? 'btn-primary' : 'btn-ghost' }}"
+        @click="Livewire.dispatchTo('pages.inventory', 'switchTab', { button: 'product-category' })"
+        > Product Categories
+    </x-button>
+
+    <x-button 
+        id="transactions" 
+        variant="ghost" 
+        class="{{ ($activeTabButton === 'transactions') ? 'btn-primary' : 'btn-ghost' }}"
+        @click="Livewire.dispatchTo('pages.inventory', 'switchTab', { button: 'transactions' })"
+        > Transactions 
+    </x-button>
+
+    <x-button 
+        id="suppliers" 
+        variant="ghost" 
+        class="{{ ($activeTabButton === 'suppliers') ? 'btn-primary' : 'btn-ghost' }}"
+        @click="Livewire.dispatchTo('pages.inventory', 'switchTab', { button: 'suppliers' })"
+        > Suppliers 
+    </x-button>
 </div>
