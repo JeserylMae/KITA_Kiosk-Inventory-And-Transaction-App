636951639ba466f2a159433e3ec8b0fd94bb78aa
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-09-01 23:48:32 +0800

    Fixed inventory dropdown. FINISHED

--- kita-src/resources/views/livewire/navigations/sidebar.blade.php
+++ kita-src/resources/views/livewire/navigations/sidebar.blade.php
@@ -48,6 +48,42 @@
     </x-button>
 
     @if (request()->routeIs('page.inventory'))
-        <livewire:pages.inventory class="order-3"/>
+        <div class="order-3 w-full pl-5 ml-3 border-l border-l-[var(--color-border)]">
+            <x-button 
+                id="products" 
+                variant="filled" 
+                wire:navigate
+                href="{{ route('page.dashboard') }}"
+                class="{{ Route::is('page.products') ? 'sidebar-item-active' : 'sidebar-item' }}"
+                > Products 
+            </x-button>
+
+            <x-button 
+                id="product-category" 
+                variant="filled" 
+                wire:navigate
+                href="{{ route('page.dashboard') }}"
+                class="{{ Route::is('page.product-category') ? 'sidebar-item-active' : 'sidebar-item' }}"
+                > Product Categories
+            </x-button>
+
+            <x-button 
+                id="transactions" 
+                variant="filled" 
+                wire:navigate
+                href="{{ route('page.dashboard') }}"
+                class="{{ Route::is('page.transactions') ? 'sidebar-item-active' : 'sidebar-item' }}"
+                > Transactions 
+            </x-button>
+
+            <x-button 
+                id="suppliers" 
+                variant="filled" 
+                wire:navigate
+                href="{{ route('page.dashboard') }}"
+                class="{{ Route::is('page.supplier') ? 'sidebar-item-active' : 'sidebar-item' }}"
+                > Suppliers 
+            </x-button>
+        </div>
     @endif
 </div>
