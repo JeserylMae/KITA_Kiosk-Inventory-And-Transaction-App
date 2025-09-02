ca3a621db1582a13c885b7ad50d6c5950abcea18
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-09-02 23:00:46 +0800

    Defined fillable attributes of the component. FINISHED

--- kita-src/app/Livewire/Components/DropdownCheck.php
+++ kita-src/app/Livewire/Components/DropdownCheck.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class DropdownCheck extends Component
 {
+
+    public string $label;
+    public array $option = [];
+
     public function render()
     {
         return view('livewire.components.dropdown-check');
