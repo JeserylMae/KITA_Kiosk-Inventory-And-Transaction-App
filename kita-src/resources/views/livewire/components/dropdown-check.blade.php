20ba60ee287cc8bd65381a46c9643e28dbbbcd0e
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-09-02 23:01:02 +0800

    Defined the dropdown component. FINISHED

--- kita-src/resources/views/livewire/components/dropdown-check.blade.php
+++ kita-src/resources/views/livewire/components/dropdown-check.blade.php
@@ -1,3 +1,12 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-</div>
+
+@if (count($option) !== 0)
+    <flux:dropdown class="checkbox-items">
+        <flux:button icon:trailing="chevron-down"> {{ $label }} </flux:button>
+        
+        <flux:menu>
+            @foreach ($option as $item)
+                <flux:menu.checkbox>{{ $item }}</flux:menu.checkbox>
+            @endforeach
+        </flux:menu>
+    </flux:dropdown>
+@endif
