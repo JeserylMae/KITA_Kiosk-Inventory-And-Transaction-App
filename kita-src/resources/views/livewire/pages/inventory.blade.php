4ab9c55ff5a6382c7afd841abc9c1a67c81ac453
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-09-02 23:02:26 +0800

    Defined the filters. FINISHED

--- kita-src/resources/views/livewire/pages/inventory.blade.php
+++ kita-src/resources/views/livewire/pages/inventory.blade.php
@@ -12,21 +12,26 @@
 
     <x-inventory-dropdown class="inline-start !border-0 !m-0 !mt-6 !p-0" activeTabButton="{{ $activeTabButton }}"/>
 
-    <div id="products" class="card stack-start w-full">
+    <div id="products" class="card stack-start w-full gap-4">
+        <div class="inline-between w-full">
+            <div class="inline-start items-center gap-4">
+                <h3 class="text-[var(--color-muted-foreground)] font-normal">Filter By:</h3>
+
+                <livewire:components.dropdown-check label="Products" :option="$productOptions"/>
+                <livewire:components.dropdown-check label="Brands" :option="$brandOptions"/>
+                <livewire:components.dropdown-check label="Category" :option="$categoryOptions"/>
+                <livewire:components.dropdown-check label="Selling Price" :option="$sellingPriceOptions"/>
+            </div>
+
+            {{-- Buttons --}}
+        </div>
+
         <livewire:components.table
-            :headers="[
-                'Product', 
-                'Brand', 
-                'Quantity', 
-                'Category', 
-                'Purchase Price', 
-                'Selling Price', 
-                'Expiry'
-            ]"
+            :headers="[ 'Product', 'Brand', 'Quantity', 'Category', 'Purchase Price', 'Selling Price']"
             :rows="[
-                ['Ice Cream', 'ABC', '230', 'Food', '₱ 4,3432.34', '₱ 45.00', 'Jan 25, 2027'],
-                ['Bread', 'ABC', '21', 'Food', '₱ 432.34', '₱ 50.00', 'Jan 25, 2027'],
-                ['Ice Cream', 'ABC', '230', 'Food', '₱ 4,3432.34', '₱ 45.00', 'Jan 25, 2027'],
+                ['Ice Cream', 'ABC', '230', 'Food', '₱ 4,3432.34', '₱ 45.00'],
+                ['Bread', 'ABC', '21', 'Food', '₱ 432.34', '₱ 50.00'],
+                ['Ice Cream', 'ABC', '230', 'Food', '₱ 4,3432.34', '₱ 45.00'],
             ]"
             empty="No items were invoiced in this time frame."
             checkbox="true"
